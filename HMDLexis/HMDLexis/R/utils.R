81d16e10482971fc4e9d158b919331d0dd57e1fd
triffe <triffe@demog.berkeley.edu>
2015-05-26 19:54:01 -0700

    add date handling to utils. Now removed of lubridate dependency

--- HMDLexis/HMDLexis/R/utils.R
+++ HMDLexis/HMDLexis/R/utils.R
@@ -50,6 +50,146 @@ NULL
 
 
 
+#'
+#' @title a function to determine whether a year is a leap year. 
+#' 
+#' @description In order to remove lubridate dependency, we self-detect leap years and adjust February accordingly.
+#' 
+#' @param Year integer of year to query
+#' 
+#' @return logical is the Year a leap year or not
+#' 
+#' @export
+
+isLeapYear <- function (Year){      # CB: mostly good algorithm from wikipedia
+  ifelse(
+    ( (Year %% 4) == 0  &  (Year %% 100) != 0   ) | ( (Year %% 400) == 0 ),
+    TRUE, FALSE )
+}
+#years <- 1700:2200
+#all(lubridate::leap_year(years) == isLeapYear(years))
+
+#'
+#' @title ypart function to determine the proportion of a year passed as of a particular date
+#' 
+#' @description The fraction returned by this is used e.g. for intercensal estimates. Function uses 'lubridate' package to handle dates elegantly.
+#' 
+#' @param Year 4-digit year (string or integer)
+#' @param Month month digits (string or integer, 1 or 2 characters)
+#' @param Day Day of month digits (string or integer, 1 or 2 characters)
+#' @param reproduce.matlab logical. Default TRUE. Assume 365 days in a year.
+#' @param detect.mid.year logical. if \code{TRUE}, June 30 or July 1 will always return .5.
+#' @param detect.start.end logical. default \code{TRUE}. Should Jan 1 always be 0 and Dec 31 always be 1?
+#' 
+# #' @importFrom lubridate ymd
+# #' @importFrom lubridate floor_date
+# #' @importFrom lubridate yday
+#' 
+#' @export
+#' 
+
+ypart <- function(Year, Month, Day, reproduce.matlab = TRUE, detect.mid.year = FALSE, detect.start.end = TRUE){
+  M <- c(0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334)
+  if (reproduce.matlab){
+    Day   <- as.integer(Day)
+    Month <- as.integer(Month)
+    if (is.na(Day) & is.na(Month)){
+      return(.5)
+    }
+    if (Day == 1 & Month == 1){
+      return(0)
+    }
+    return((M[Month] + Day) / 365)
+  }
+  # this chunk written just to avoiding writing p_my()
+  if (detect.mid.year){
+    .d <- as.integer(Day)
+    .m <- as.integer(Month)
+    if ((.d == 30 & .m == 6) | (.d == 1 & .m == 7)){
+      return(.5)
+    }
+  }
+  
+  if (detect.start.end){
+    .d <- as.integer(Day)
+    .m <- as.integer(Month)
+    if (.d == 1 & .m == 1){
+      return(0)
+    }
+    if(.d == 31 & .m == 12){
+      return(1)
+    }
+  }
+  
+  monthdur    <- diff(c(M,365))
+  monthdur[2] <- monthdur[2] + isLeapYear(Year)
+  M           <- cumsum(monthdur) - 31
+  return((M[Month] + Day) / sum(monthdur))
+  
+  # get into date class
+#  Date  <- ymd(paste(Year, Month, Day, sep = "/"))
+#  # what was jan 1st?
+#  Jan1  <- floor_date(Date, "year") 
+#  # how many days have passed?
+#  Days  <- yday(Date) - 1
+#  # if we want to account for possible leap years, we get next year's Jan 1st (our Dec 31st)
+#  Dec31 <- floor_date(ymd(paste(Year + 1, Month, Day, sep = "/")), "year") 
+#  # and the difference is the year length
+#  Denom <- as.integer(Dec31 - Jan1)
+#  # only gives same as matlab on Jan 1st.
+#  Days / Denom
+}
+
+
+#'
+#' @title yint get interval as fraction of full years
+#' 
+#' @description Either assume 365 days in the year, or get the precise duration.
+#' 
+#' @param Day1 Day of first date
+#' @param Month1 Month of first date
+#' @param Year1 Year of first date
+#' @param Day1 Day of second date
+#' @param Month1 Month of second date
+#' @param Year1 Year of second date
+#' @param reproduce.matlab logical. default \code{TRUE}. Assume 365 days in all years?
+#' @param detect.mid.year logical. default \code{FALSE}. Should June 30 and July 1 be considered .5?
+#' @param detect.start.end logical. default \code{TRUE}. Should Jan 1 always be 0 and Dec 31 always be 1?
+#' 
+#' @return decimal value of year fraction (can be greater than 1)
+#' 
+# #' @importFrom lubridate decimal_date
+# #' @importFrom lubridate ymd
+#' 
+#' @export
+#' 
+yint <- function(Day1, Month1, Year1, Day2, Month2, Year2, reproduce.matlab = TRUE, 
+  detect.mid.year = FALSE, detect.start.end = TRUE){
+  if (reproduce.matlab){
+    return(abs(Year1 - Year2 + (Day1 - Day2) / 365 +  (Month1 - Month2) / 12))
+  }
+  
+  # we can be more exacting, if desired:
+#  abs(decimal_date(ymd(paste0(Year1, sprintf("%02d", Month1), sprintf("%02d", Day1)))) -
+#      decimal_date(ymd(paste0(Year2, sprintf("%02d", Month2), sprintf("%02d", Day2)))))
+#  
+  Ypart1 <- ypart(Year = Year1, 
+    Month = Month1, 
+    Day = Day1, 
+    reproduce.matlab = reproduce.matlab, 
+    detect.mid.year = detect.mid.year, 
+    detect.start.end = detect.start.end)
+  Ypart2 <- ypart(Year = Year2, 
+    Month = Month2, 
+    Day = Day2, 
+    reproduce.matlab = reproduce.matlab, 
+    detect.mid.year = detect.mid.year, 
+    detect.start.end = detect.start.end)
+#  
+  (1 - Ypart1) + abs(Year2 - Year1) + Ypart2
+  
+}
+
 
 
 
