5957830778620982fc2e0603c85a271f6d5ccd7a
triffe <triffe@demog.berkeley.edu>
2016-05-24 07:37:50 -0700

    added check for negative pop counts. if above age 115 just replace with 0s, but if below abort with prejudice

--- HMDLexis/HMDLexis/R/p_postcensal.R
+++ HMDLexis/HMDLexis/R/p_postcensal.R
@@ -208,10 +208,36 @@ p_postcensal <- function(Pop, Deaths, Births, MPVERSION = 5, reproduce.matlab = 
     Ps$RefCode     <- Ps$NoteCode2 <- Ps$NoteCode3 <- NA
     Ps             <- Ps[, colnames(C1s)]
     Ps$Cohort      <- NULL
+    
+    # TR: added 24.05.2016
+    # negative numbers are possible in high ages. I'm going to make an executive decision
+    # here. If it's above age 115, we can replace negatives with 0,
+    # but if a negative occurs below age 115 then we'll abort with predjudice and
+    # an informative warning message.
+    
+    if (any(Ps$Population < 0)){
+      AgesNeg <- Ps$Agei[Ps$Population < 0]
+      if (!all(AgesNeg >= 115)){
+        cat("\npostcensal estimates have some negative population counts in ages under 115
+We therefore abort this function. Either there was a negative pop count you need to deal with, 
+or there are deaths being deducted from a zero or small pop count and dropping
+below zero. If that is the case, and this is a minor data quality issue, then deal with
+it prior to calling this function.")
+       return(Pop)
+      }
+     
+      Ps$Population[Ps$Population < 0] <- 0
+    }
+    
+    
     PopOut[[Sex]]  <- Ps
     
   }
   PopOut        <- do.call(rbind, PopOut)
+  
+
+  
+  
   
   PopOut        <- resortPops(PopOut)
   invisible(PopOut)
