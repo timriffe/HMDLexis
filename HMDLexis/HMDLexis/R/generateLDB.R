359d1d222a18d57682c5efe98a0f61e2b8fbb9e6
triffe <triffe@demog.berkeley.edu>
2016-05-26 01:22:56 -0700

    added writeLDB copied from /data/commons/boe/Triffe_git/HMDLexis/HMDLexis/HMDLexis/R
    I added comments in the function body, as there are things I would
    change.

--- HMDLexis/HMDLexis/R/generateLDB.R
+++ HMDLexis/HMDLexis/R/generateLDB.R
@@ -278,6 +278,71 @@ testAgeMatch<- function(ages1, ages2, desc, tyear=NULL, tsex=NULL){
   return(res)
 }
 
+
+#' @title write out LexisDB
+#' @description This is a beta function.
+#' @return function called for its side effects: some files written.
+#' @export 
+###############################################################################
+writeLDB <- function(ldb.by.sex, 
+  LDBpath,  # WORKING/XXX/LexisDB   typically
+  COUNTRY=NULL,
+  #save.bin = FALSE,
+  strict = TRUE){
+  
+  dir.create(LDBpath,recursive=TRUE, mode="0775")
+  
+  
+  # TR 26 May, 2016: 
+  # 1) use argument reproduce.matlab to toggle whether 
+  # matlab style rounding is used. Default should be FALSE,
+  # and TRUE only for testing. R uses an international standard
+  # 2) use the same folder find/make code that you elsewhere in the LifeTable scripts.
+  # 3) move the matlabrounding function to the utils.R file and document it...
+  
+  # MatlabRound() is for rounding output, should give same result as matlab, assuming that's important
+  # by CB, updated by TR to take digits as arg.
+  # CAB.  Fixed rounding formula to handle negative numbers
+  MatlabRoundLDB <- function(x, digits = 0){ 
+    
+    # this 1) rounds, and
+    # 2) makes sure that the zeros stay on the end of the number string to the specified number of digits
+    
+    ## TODO: use tapply to ensure that any rounding sums to total?
+    if (is.numeric(x)){
+      fac <- rep(10 ^ digits, length(x))
+      #x   <-  floor(x * fac + sign(x) * 0.5) / fac
+      x   <-  sign(x) * ( floor( abs(x) * fac + 0.5)/ fac )
+    }
+    return(x)
+  }
+  
+  if(is.null(COUNTRY))
+    COUNTRY<- ldb.by.sex$COUNTRY
+  
+  ldb.f <- ldb.by.sex[["f"]]
+  ldb.m <- ldb.by.sex[["m"]]
+  
+  # TR: 26 May, 2016: 1) use reproduce.matlab to toggle this behavior.
+  # 2) have a separate argument for whether and how much to round.
+  if(strict){
+    ldb.f$Population <- MatlabRoundLDB(ldb.f$Population, digits=2)
+    ldb.f$Deaths <- MatlabRoundLDB(ldb.f$Deaths, digits=2)
+    ldb.m$Population <- MatlabRoundLDB(ldb.m$Population, digits=2)
+    ldb.m$Deaths <- MatlabRoundLDB(ldb.m$Deaths, digits=2)
+  } 
+  
+  write.csv(ldb.f, 
+    file=file.path(LDBpath, paste0("f",COUNTRY,".txt") ),
+    eol="\r\n",
+    row.names=FALSE)
+  write.csv(ldb.m, 
+    file=file.path(LDBpath, paste0("m",COUNTRY,".txt") ),
+    eol="\r\n",
+    row.names=FALSE)    
+  
+}
+
 ## Q's  
 ## (1) do Tadj  ever really enter?   Are they not used prior to LexisDB output?  Seems bad
 ##     to mix adjustment of data with output of data
